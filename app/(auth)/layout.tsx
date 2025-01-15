f62d8627cf1e7575fdcaeee6fbcb68b159078910
S4v!0r <gbp17@outlook.com>
2025-01-14 22:40:38 -0500

    refactor auth layout to enhance animations and update Tailwind CSS configuration

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,8 +13,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: '0010022200025.jpg',
-        width: 800,
-        height: 600,
         alt: 'Teacher Ranker Logo',
       },
     ],
@@ -29,15 +27,15 @@ export default function AuthLayout({
   return (
     <div className="min-h-screen w-full relative bg-[#f6f6ee] overflow-hidden">
       {/* Background circles */}
-      <div className="absolute top-0 right-16 w-44 h-44 md:w-64 md:h-64 bg-[#FFA54E] rounded-full opacity-50 translate-x-1/2 -translate-y-1/2" />
-      <div className="absolute top-0 right-40 md:right-48 w-24 h-24 md:w-40 md:h-40 bg-[#FFC941] rounded-full opacity-30 translate-x-1/2 -translate-y-1/2" />
+      <div className="absolute top-0 right-16 w-44 h-44 md:w-64 md:h-64 bg-[#FFA54E] rounded-full opacity-50 translate-x-1/2 -translate-y-1/2 animate-move-bg" />
+      <div className="absolute top-0 right-40 md:right-48 w-24 h-24 md:w-40 md:h-40 bg-[#FFC941] rounded-full opacity-30 translate-x-1/2 -translate-y-1/2 animate-move-bg" />
 
-      <div className="absolute bottom-24 left-20 w-64 h-64 bg-[#FFA54E] rounded-full opacity-50 -translate-x-1/2 translate-y-1/2" />
-      <div className="absolute bottom-0 left-52 w-40 h-40 bg-[#FFC941] rounded-full opacity-30 -translate-x-1/2 translate-y-1/2" />
+      <div className="absolute bottom-24 left-20 w-64 h-64 bg-[#FFA54E] rounded-full opacity-50 -translate-x-1/2 translate-y-1/2 animate-move-bg" />
+      <div className="absolute bottom-0 left-52 w-40 h-40 bg-[#FFC941] rounded-full opacity-30 -translate-x-1/2 translate-y-1/2 animate-move-bg" />
 
       <div className="container mx-auto px-4 py-8 min-h-screen flex flex-col lg:flex-row items-center justify-center gap-12 lg:gap-48">
         {/* Left side - Logo and tagline */}
-        <div>
+        <div className="text-center lg:text-left flex flex-col items-center animate-move-up delay-[0.1s]">
           <div className="mx-auto lg:mx-0 w-48 h-48 rounded-full bg-[rgba(188,107,242,0.16)] flex items-center justify-center mb-6">
             <Image
               src={`/teacher-ranker-logo.svg`}
